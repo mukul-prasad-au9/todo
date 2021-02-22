e61473c0c953ba62dd004bbaf4d3eee123263f8e
mukul-prasad-au9 <mukulp0783@gmail.com>
2021-02-22 21:51:56 +0530

    everything done only db connection is left

--- routes/route.js
+++ routes/route.js
@@ -2,10 +2,31 @@ const user = require("../model")
 const express =require("express")
 const router = express.Router()
 
-
+task=[{
+    title:"todo",
+    todo:"we have to do it",
+    progress:"ongoing"
+},
+{
+    title:"todo",
+    todo:"we have to do it",
+    progress:"ongoing"
+},
+{
+    title:"todo",
+    todo:"we have to do it",
+    progress:"completed"
+},
+{
+    title:"todo",
+    todo:"we have to do it",
+    progress:"completed"
+}
+]
 
 router.get("/todo",(req,res)=>{
-    res.render("todo")
+    let color = "primary";
+    res.render("todo",{task,color})
 })
 
 
